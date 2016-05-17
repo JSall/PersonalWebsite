f2e07fd97e0224298dfcbc8d10b443b6ddc46462
JSall <jas11v@my.fsu.edu>
2016-05-17 12:48:05 +0100

    cv updates

--- PersonalWebsite.Web/Controllers/CVController.cs
+++ PersonalWebsite.Web/Controllers/CVController.cs
@@ -26,7 +26,7 @@ namespace PersonalWebsite.Web.Controllers
 
         public ActionResult DownloadCV()
         {
-            string filename = "CV.docx";
+            string filename = "CV.pdf";
             string filepath = AppDomain.CurrentDomain.BaseDirectory + "\\Content\\" + filename;
             byte[] filedata = System.IO.File.ReadAllBytes(filepath);
             string contentType = MimeMapping.GetMimeMapping(filepath);
